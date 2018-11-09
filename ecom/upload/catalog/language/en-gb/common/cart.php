4ff677383ba53ce65409ea44d2f2c35a006e35cf
kushal-goenka <kushalgoenka97@gmail.com>
2018-11-09 16:09:55 -0600

    Cleared Old code and added template to start

--- ecom/upload/catalog/language/en-gb/common/cart.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['text_items']     = '<span class="cart_item">(%s) items</span><span class="price">%s</span>';
-$_['text_empty']     = 'Your shopping cart is empty!';
-$_['text_cart']      = 'View Cart';
-$_['text_checkout']  = 'Checkout';
-$_['text_recurring'] = 'Payment Profile';
-$_['text_shopping']	 = 'Shopping Cart';
